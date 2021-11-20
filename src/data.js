b9dfabf80b7d66bce1b147147c10c167effd7603
yusuf <ysfozturk3@gmail.com>
2021-11-20 20:55:05 +0100

    added categories

--- src/data.js
+++ src/data.js
@@ -9,14 +9,16 @@ export const sliderItems = [
     },
     {
       id: 2,
-      img: "https://i.ibb.co/DG69bQ4/2.png",
+      // img: "https://i.ibb.co/DG69bQ4/2.png",
+      img: "https://www.pngarts.com/files/12/Barbara-Palvin-Model-PNG-Image.png",
       title: "AUTUMN COLLECTION",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fcf1ed",
     },
     {
       id: 3,
-      img: "https://i.ibb.co/cXFnLLV/3.png",
+      // img: "https://i.ibb.co/cXFnLLV/3.png",
+      img:"https://www.pngarts.com/files/2/Model-PNG-High-Quality-Image.png",
       title: "LOUNGEWEAR LOVE",
       desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
       bg: "fbf0f4",
