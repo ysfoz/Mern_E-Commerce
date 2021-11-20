ded58c647e9a3d4ca091bb74d008cad1b661d03a
yusuf <ysfozturk3@gmail.com>
2021-11-21 00:14:05 +0100

    home page styled completed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
 import Products from "../components/Products";
 import Slider from "../components/Slider";
+import Footer from "../components/Footer"
 
 const Home = () => {
   return (
@@ -13,6 +15,8 @@ const Home = () => {
       <Slider />
       <Categories />
       <Products />
+      <Newsletter/>
+      <Footer/>
     </div>
   );
 };
